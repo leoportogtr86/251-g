0e6e2d92db815f6eaa06b03883e7147bcfd63409
Leo Porto <leonardoreisporto@gmail.com>
2020-06-14 16:00:53 -0300

    Updates

---
+++ precache-manifest.41bf7cc3921a17958d4ee42b4d683412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e61091cc1a5d778b982b3f933614b9",
+    "url": "/251-g/index.html"
+  },
+  {
+    "revision": "84a4d225c2daec92a502",
+    "url": "/251-g/static/css/main.e95be56b.chunk.css"
+  },
+  {
+    "revision": "5fc22a869b0db6b4759d",
+    "url": "/251-g/static/js/2.48d779eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/251-g/static/js/2.48d779eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a4d225c2daec92a502",
+    "url": "/251-g/static/js/main.fd4ab9a3.chunk.js"
+  },
+  {
+    "revision": "9473499c101b6190d089",
+    "url": "/251-g/static/js/runtime-main.f4077934.js"
+  }
+]);
